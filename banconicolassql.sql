2ddeb19f432b8cd8890a601aeac57afd8b58e15e
Nícolas Klaus <65172437+Nicolas-boop@users.noreply.github.com>
2026-09-17 16:34:36 -0300

    Aula 17/09
    Teste banco de dados

---
+++ banconicolassql.sql
@@ -0,0 +1,39 @@
+CREATE TABLE painelnicolas (
+  idpainelnicolas INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  nome VARCHAR(50) NULL,
+  datainicio DATETIME NULL,
+  datafim DATETIME NULL,
+  linhaum VARCHAR(255) NULL,
+  linhadois VARCHAR(255) NULL,
+  PRIMARY KEY(idpainelnicolas)
+);
+
+CREATE TABLE macnicolas (
+  idmacnicolas VARCHAR(17) NOT NULL,
+  nome VARCHAR(50) NULL,
+  ativo BOOL NULL,
+  pinor INTEGER UNSIGNED NULL,
+  pinog INTEGER UNSIGNED NULL,
+  pinob INTEGER UNSIGNED NULL,
+  PRIMARY KEY(idmacnicolas)
+);
+
+CREATE TABLE painelnicolas_has_lednicolas (
+  painelnicolas_idpainelnicolas INTEGER UNSIGNED NOT NULL,
+  macnicolas_idmacnicolas VARCHAR(17) NOT NULL,
+  leda INTEGER UNSIGNED NULL,
+  ledg INTEGER UNSIGNED NULL,
+  ledb INTEGER UNSIGNED NULL,
+  INDEX painelnicolas_has_lednicolas_FKIndex1(macnicolas_idmacnicolas),
+  INDEX painelnicolas_has_lednicolas_FKIndex2(painelnicolas_idpainelnicolas),
+  FOREIGN KEY(macnicolas_idmacnicolas)
+    REFERENCES macnicolas(idmacnicolas)
+      ON DELETE NO ACTION
+      ON UPDATE NO ACTION,
+  FOREIGN KEY(painelnicolas_idpainelnicolas)
+    REFERENCES painelnicolas(idpainelnicolas)
+      ON DELETE NO ACTION
+      ON UPDATE NO ACTION
+);
+
+
