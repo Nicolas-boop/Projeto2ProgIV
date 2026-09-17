f00d6d875c6cecbf58846f6388d64a42feb145cd
Nícolas Klaus <65172437+Nicolas-boop@users.noreply.github.com>
2026-09-17 16:32:08 -0300

    Aula 17/09
    Alteração banco de dados

--- banconicolasql.sql
+++ banconicolasql.sql
@@ -1,3 +1,13 @@
+CREATE TABLE painelnicolas (
+  idpainelnicolas INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  nome VARCHAR(50) NULL,
+  datainicio DATETIME NULL,
+  datafim DATETIME NULL,
+  linhaum VARCHAR(255) NULL,
+  linhadois VARCHAR(255) NULL,
+  PRIMARY KEY(idpainelnicolas)
+);
+
 CREATE TABLE macnicolas (
   idmacnicolas VARCHAR(17) NOT NULL,
   nome VARCHAR(50) NULL,
@@ -8,29 +18,18 @@ CREATE TABLE macnicolas (
   PRIMARY KEY(idmacnicolas)
 );
 
-CREATE TABLE painelnicolas (
-  idpainelnicolas INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  macnicolas_idmacnicolas VARCHAR(17) NOT NULL,
-  nome VARCHAR(50) NULL,
-  datainicio DATETIME NULL,
-  datafim DATETIME NULL,
-  linhaum VARCHAR(255) NULL,
-  linhadois VARCHAR(255) NULL,
-  PRIMARY KEY(idpainelnicolas),
-  INDEX painelnicolas_FKIndex1(macnicolas_idmacnicolas),
-  FOREIGN KEY(macnicolas_idmacnicolas)
-    REFERENCES macnicolas(idmacnicolas)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION
-);
-
 CREATE TABLE painelnicolas_has_lednicolas (
   painelnicolas_idpainelnicolas INTEGER UNSIGNED NOT NULL,
+  macnicolas_idmacnicolas VARCHAR(17) NOT NULL,
   ledA INTEGER UNSIGNED NULL,
   ledG INTEGER UNSIGNED NULL,
   ledB INTEGER UNSIGNED NULL,
-  PRIMARY KEY(painelnicolas_idpainelnicolas),
-  INDEX painelnicolas_has_lednicolas_FKIndex1(painelnicolas_idpainelnicolas),
+  INDEX painelnicolas_has_lednicolas_FKIndex1(macnicolas_idmacnicolas),
+  INDEX painelnicolas_has_lednicolas_FKIndex2(painelnicolas_idpainelnicolas),
+  FOREIGN KEY(macnicolas_idmacnicolas)
+    REFERENCES macnicolas(idmacnicolas)
+      ON DELETE NO ACTION
+      ON UPDATE NO ACTION,
   FOREIGN KEY(painelnicolas_idpainelnicolas)
     REFERENCES painelnicolas(idpainelnicolas)
       ON DELETE NO ACTION
