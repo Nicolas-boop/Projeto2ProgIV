3847c89682fad1a58c293cbced3642362b39a5f9
Nícolas Klaus <65172437+Nicolas-boop@users.noreply.github.com>
2026-09-17 17:21:01 -0300

    Aula - 17/09
    Alterações banco de dados

--- criabanco.sql
+++ criabanco.sql
@@ -3,8 +3,6 @@ CREATE TABLE painelnicolas (
   nome VARCHAR(50) NULL,
   datainicio DATETIME NULL,
   datafim DATETIME NULL,
-  linhaum VARCHAR(255) NULL,
-  linhadois VARCHAR(255) NULL,
   PRIMARY KEY(idpainelnicolas)
 );
 
@@ -12,13 +10,20 @@ CREATE TABLE macnicolas (
   idmacnicolas VARCHAR(17) NOT NULL,
   nome VARCHAR(50) NULL,
   ativo BOOL NULL,
-  pinor INTEGER UNSIGNED NULL,
-  pinog INTEGER UNSIGNED NULL,
-  pinob INTEGER UNSIGNED NULL,
   PRIMARY KEY(idmacnicolas)
 );
 
+CREATE TABLE lednicolas (
+  idlednicolas INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  nome VARCHAR(50) NULL,
+  portar INTEGER UNSIGNED NULL,
+  portag INTEGER UNSIGNED NULL,
+  portab INTEGER UNSIGNED NULL,
+  PRIMARY KEY(idlednicolas)
+);
+
 CREATE TABLE painelnicolas_has_lednicolas (
+  lednicolas_idlednicolas INTEGER UNSIGNED NOT NULL,
   painelnicolas_idpainelnicolas INTEGER UNSIGNED NOT NULL,
   macnicolas_idmacnicolas VARCHAR(17) NOT NULL,
   leda INTEGER UNSIGNED NULL,
@@ -26,12 +31,17 @@ CREATE TABLE painelnicolas_has_lednicolas (
   ledb INTEGER UNSIGNED NULL,
   INDEX painelnicolas_has_lednicolas_FKIndex1(macnicolas_idmacnicolas),
   INDEX painelnicolas_has_lednicolas_FKIndex2(painelnicolas_idpainelnicolas),
+  INDEX painelnicolas_has_lednicolas_FKIndex3(lednicolas_idlednicolas),
   FOREIGN KEY(macnicolas_idmacnicolas)
     REFERENCES macnicolas(idmacnicolas)
       ON DELETE NO ACTION
       ON UPDATE NO ACTION,
   FOREIGN KEY(painelnicolas_idpainelnicolas)
     REFERENCES painelnicolas(idpainelnicolas)
+      ON DELETE NO ACTION
+      ON UPDATE NO ACTION,
+  FOREIGN KEY(lednicolas_idlednicolas)
+    REFERENCES lednicolas(idlednicolas)
       ON DELETE NO ACTION
       ON UPDATE NO ACTION
 );
