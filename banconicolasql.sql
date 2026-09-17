85e50a14ec20f6cd23b75f8c98afdfe234ff5035
Nícolas Klaus <65172437+Nicolas-boop@users.noreply.github.com>
2026-09-17 16:25:57 -0300

    Aula 17/09
    Alteração banco de dados.

--- banconicolasql.sql
+++ banconicolasql.sql
@@ -2,16 +2,10 @@ CREATE TABLE macnicolas (
   idmacnicolas VARCHAR(17) NOT NULL,
   nome VARCHAR(50) NULL,
   ativo BOOL NULL,
+  pinoR INTEGER UNSIGNED NULL,
+  pinoG INTEGER UNSIGNED NULL,
+  pinoB INTEGER UNSIGNED NULL,
   PRIMARY KEY(idmacnicolas)
-);
-
-CREATE TABLE lednicolas (
-  idlednicolas INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  nome VARCHAR(50) NULL,
-  portaR INTEGER UNSIGNED NULL,
-  portaG INTEGER UNSIGNED NULL,
-  portaB INTEGER UNSIGNED NULL,
-  PRIMARY KEY(idlednicolas)
 );
 
 CREATE TABLE painelnicolas (
@@ -32,19 +26,13 @@ CREATE TABLE painelnicolas (
 
 CREATE TABLE painelnicolas_has_lednicolas (
   painelnicolas_idpainelnicolas INTEGER UNSIGNED NOT NULL,
-  lednicolas_idlednicolas INTEGER UNSIGNED NOT NULL,
   ledA INTEGER UNSIGNED NULL,
   ledG INTEGER UNSIGNED NULL,
   ledB INTEGER UNSIGNED NULL,
-  PRIMARY KEY(painelnicolas_idpainelnicolas, lednicolas_idlednicolas),
+  PRIMARY KEY(painelnicolas_idpainelnicolas),
   INDEX painelnicolas_has_lednicolas_FKIndex1(painelnicolas_idpainelnicolas),
-  INDEX painelnicolas_has_lednicolas_FKIndex2(lednicolas_idlednicolas),
   FOREIGN KEY(painelnicolas_idpainelnicolas)
     REFERENCES painelnicolas(idpainelnicolas)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION,
-  FOREIGN KEY(lednicolas_idlednicolas)
-    REFERENCES lednicolas(idlednicolas)
       ON DELETE NO ACTION
       ON UPDATE NO ACTION
 );
