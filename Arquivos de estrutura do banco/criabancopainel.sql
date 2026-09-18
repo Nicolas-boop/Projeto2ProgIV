3ea45853acf1caf41b2689d737b02cf01abe83a2
Nícolas Klaus <65172437+Nicolas-boop@users.noreply.github.com>
2026-09-18 14:43:35 -0300

    Aula - 18/09
    sexta ou sétima alteração

--- Arquivos de estrutura do banco/criabancopainel.sql
+++ Arquivos de estrutura do banco/criabancopainel.sql
@@ -1,17 +1,17 @@
+CREATE TABLE macnicolas (
+  idmacnicolas VARCHAR(17)  NOT NULL  ,
+  nome VARCHAR(35)  NULL  ,
+  ativo BOOL  NULL    ,
+PRIMARY KEY(idmacnicolas));
+
+
+
 CREATE TABLE painelnicolas (
   idpainelnicolas INTEGER UNSIGNED  NOT NULL   AUTO_INCREMENT,
   nome VARCHAR(50)  NULL  ,
   datainicio DATETIME  NULL  ,
   datafim DATETIME  NULL    ,
 PRIMARY KEY(idpainelnicolas));
-
-
-
-CREATE TABLE macnicolas (
-  idmacnicolas VARCHAR(17)  NOT NULL  ,
-  nome VARCHAR(35)  NULL  ,
-  ativo BOOL  NULL    ,
-PRIMARY KEY(idmacnicolas));
 
 
 
@@ -25,27 +25,28 @@ PRIMARY KEY(idlednicolas));
 
 
 
-CREATE TABLE macnicolas_has_lednicolas (
-  macnicolas_idmacnicolas VARCHAR(17)  NOT NULL  ,
+CREATE TABLE combinanicolas (
+  idcombinanicolas INTEGER UNSIGNED  NOT NULL   AUTO_INCREMENT,
   lednicolas_idlednicolas INTEGER UNSIGNED  NOT NULL  ,
   painelnicolas_idpainelnicolas INTEGER UNSIGNED  NOT NULL  ,
+  macnicolas_idmacnicolas VARCHAR(17)  NOT NULL  ,
   ledr INTEGER UNSIGNED  NULL  ,
   ledg INTEGER UNSIGNED  NULL  ,
   ledb INTEGER UNSIGNED  NULL    ,
-PRIMARY KEY(macnicolas_idmacnicolas, lednicolas_idlednicolas)  ,
-INDEX macnicolas_has_lednicolas_FKIndex1(macnicolas_idmacnicolas)  ,
-INDEX macnicolas_has_lednicolas_FKIndex2(lednicolas_idlednicolas)  ,
-INDEX macnicolas_has_lednicolas_FKIndex3(painelnicolas_idpainelnicolas),
-  FOREIGN KEY(macnicolas_idmacnicolas)
-    REFERENCES macnicolas(idmacnicolas)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION,
+PRIMARY KEY(idcombinanicolas)  ,
+INDEX combinanicolas_FKIndex1(lednicolas_idlednicolas)  ,
+INDEX combinanicolas_FKIndex2(painelnicolas_idpainelnicolas)  ,
+INDEX combinanicolas_FKIndex3(macnicolas_idmacnicolas),
   FOREIGN KEY(lednicolas_idlednicolas)
     REFERENCES lednicolas(idlednicolas)
       ON DELETE NO ACTION
       ON UPDATE NO ACTION,
   FOREIGN KEY(painelnicolas_idpainelnicolas)
     REFERENCES painelnicolas(idpainelnicolas)
+      ON DELETE NO ACTION
+      ON UPDATE NO ACTION,
+  FOREIGN KEY(macnicolas_idmacnicolas)
+    REFERENCES macnicolas(idmacnicolas)
       ON DELETE NO ACTION
       ON UPDATE NO ACTION);
 
