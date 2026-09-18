38645a7eac2ec63d2297364dee9cc9fd29bd3cd9
Nícolas Klaus <65172437+Nicolas-boop@users.noreply.github.com>
2026-09-18 14:12:24 -0300

    Aula - 18/09
    Mais uma alteração...

---
+++ Arquivos de estrutura do banco/criabancopainel.sql
@@ -0,0 +1,54 @@
+CREATE TABLE painelnicolas (
+  idpainelnicolas INTEGER UNSIGNED  NOT NULL   AUTO_INCREMENT,
+  nome VARCHAR(50)  NULL  ,
+  datainicio DATETIME  NULL  ,
+  datafim DATETIME  NULL    ,
+PRIMARY KEY(idpainelnicolas));
+
+
+
+CREATE TABLE macnicolas (
+  idmacnicolas VARCHAR(17)  NOT NULL  ,
+  nome VARCHAR(35)  NULL  ,
+  ativo BOOL  NULL    ,
+PRIMARY KEY(idmacnicolas));
+
+
+
+CREATE TABLE lednicolas (
+  idlednicolas INTEGER UNSIGNED  NOT NULL   AUTO_INCREMENT,
+  nome VARCHAR(35)  NULL  ,
+  pinor INTEGER UNSIGNED  NULL  ,
+  pinog INTEGER UNSIGNED  NULL  ,
+  pinob INTEGER UNSIGNED  NULL    ,
+PRIMARY KEY(idlednicolas));
+
+
+
+CREATE TABLE macnicolas_has_lednicolas (
+  macnicolas_idmacnicolas VARCHAR(17)  NOT NULL  ,
+  lednicolas_idlednicolas INTEGER UNSIGNED  NOT NULL  ,
+  painelnicolas_idpainelnicolas INTEGER UNSIGNED  NOT NULL  ,
+  ledr INTEGER UNSIGNED  NULL  ,
+  ledg INTEGER UNSIGNED  NULL  ,
+  ledb INTEGER UNSIGNED  NULL    ,
+PRIMARY KEY(macnicolas_idmacnicolas, lednicolas_idlednicolas)  ,
+INDEX macnicolas_has_lednicolas_FKIndex1(macnicolas_idmacnicolas)  ,
+INDEX macnicolas_has_lednicolas_FKIndex2(lednicolas_idlednicolas)  ,
+INDEX macnicolas_has_lednicolas_FKIndex3(painelnicolas_idpainelnicolas),
+  FOREIGN KEY(macnicolas_idmacnicolas)
+    REFERENCES macnicolas(idmacnicolas)
+      ON DELETE NO ACTION
+      ON UPDATE NO ACTION,
+  FOREIGN KEY(lednicolas_idlednicolas)
+    REFERENCES lednicolas(idlednicolas)
+      ON DELETE NO ACTION
+      ON UPDATE NO ACTION,
+  FOREIGN KEY(painelnicolas_idpainelnicolas)
+    REFERENCES painelnicolas(idpainelnicolas)
+      ON DELETE NO ACTION
+      ON UPDATE NO ACTION);
+
+
+
+
